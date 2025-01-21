e281bac1a69f4d20eca5bb8fa9e4c151a21cb4ed
Ahmadjonov Abduboriy <ahmadjonovabduboriy3@gmail.com>
2025-01-22 00:21:58 +0500

    Changing fetchs url again

--- src/App.jsx
+++ src/App.jsx
@@ -66,7 +66,7 @@ const App = () => {
         }),
       });
     } else {
-      telegram.sendData(JSON.stringify({ product: cartItems, queryID: queryID }));
+      telegram.sendData(JSON.stringify(cartItems));
     }
   }, [cartItems]);
 
